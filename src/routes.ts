58ce09f6ae5721048b57a6e60f04dd40a379fc13
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-02 23:32:40 +0900

    Update routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,19 @@
 import * as React from 'react-dom';
+import * as firebase from 'firebase';
 
+import AdminBoard from './components/pages/AdminBoard';
 import Html from './components/Html'
 import LoginForm from './components/organisms/LoginForm';
+
+const config = {
+	apiKey: 'AIzaSyC5c_mdvsEHCia1wf_lqCaIZQWS8lBh9Bk',
+	authDomain: 'resume-7a6c3.firebaseapp.com',
+	databaseURL: 'https://resume-7a6c3.firebaseio.com',
+	storageBucket: 'resume-7a6c3.appspot.com',
+	messagingSenderId: '103183460643'
+}
+
+firebase.initializeApp(config);
 
 export default [
 	{
@@ -15,5 +27,15 @@ export default [
 				path: '/login',
 				action: () => LoginForm()
 			},
-		]}
+		  {
+				path: '/admin',
+				action: ({ redirect }) => {
+					if (firebase.auth().currentUser) {
+						return AdminBoard();
+					}
+					return redirect('/login');
+				}
+			},
+		]
+	}
 ];
--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,19 @@ import * as express from 'express';
 import * as firebase from 'firebase';
 
 import ReactDOM from 'react-dom';
+import Router from 'universal-router';
 import debugFactory from 'debug';
 import { renderToString } from 'react-dom/server';
-import { resolve } from 'universal-router';
 import routes from './routes';
 
 const app = express();
 const debug = debugFactory('info');
 
-app.get('*', async (req, res) => {
-	const component = await resolve(routes, { path: req.url });
+app.use(async (req, res) => {
+	const component = await Router.resolve(routes, {
+		path: req.path,
+		redirect: (path) => res.redirect(path),
+	});
 	res.send(renderToString(component));
 });
 
