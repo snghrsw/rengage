68df54afc55127fdf85ce1283937836b972c1ca7
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-03 02:08:27 +0900

    Update LoginForm to post form

--- src/components/organisms/LoginForm.tsx
+++ src/components/organisms/LoginForm.tsx
@@ -3,13 +3,15 @@ import * as React from 'react';
 export default function LoginForm() {
 	return (
 		<div id="login-form">
-			<label>メールアドレス</label>
-			<input type="text" />
+			<form action="auth" method="POST">
+				<label>メールアドレス</label>
+				<input type="text" name="username" />
 
-			<label>パスワード</label>
-			<input type="password" />
+				<label>パスワード</label>
+				<input type="password" name="password" />
 
-			<input type="submit" value="送信" />
+				<input type="submit" value="送信" />
+			</form>
 		</div>
 	)
 }
