81b0a56fd8c44e08a979761c539cf89cf34a0343
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-03 01:35:34 +0900

    Fix webpack configs

---
+++ config/webpack.client.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+		bundle: "./src/client.ts",
+		vendor: ['react', 'redux', 'react-dom', 'universal-router', 'firebase']
+	},
+  output: {
+    filename: "./public/[name].js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  target: 'web',
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin("vendor")
+  ],
+	cache: true,
+	module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ]
+      }
+    ]
+  }
+};
