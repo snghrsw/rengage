8a4be692de5dbe457cde0aa677e484c0b6101565
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-07 04:26:27 +0900

    Fix syntax

--- src/components/pages/AdminBoard.tsx
+++ src/components/pages/AdminBoard.tsx
@@ -1,9 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
+
+function Test() {
+  return <div>test</div>;
+}
 
 export default function AdminBoard() {
-	return (
-		<div>
-			<h2>管理画面</h2>
-		</div>
-	);
+  return (
+    <div>
+      <h2>管理画面</h2>
+      {[1, 2, 3].map((num, index) => <Test key={index} />)}
+    </div>
+  );
 }
