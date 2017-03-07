4c8b5eda3670f69e0e1bf0b15e4e5efa5bcd6b5a
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-08 03:49:41 +0900

    Fix routes for customer

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,46 @@
 import AdminBoard from './components/pages/AdminBoard';
+import ApplicateForm from './components/organisms/ApplicateForm';
 import LoginForm from './components/organisms/LoginForm';
-import { isAuthLogin } from './server/firebase';
+import Resume from './components/organisms/Resume';
+import Waiting from './components/pages/Waiting';
+import { auth } from './server/firebase';
 
 export default [
   {
+    path: '/:uid',
+    action: ({ redirect }) => {
+      if (!auth.isSignedCustomer) {
+        return redirect('/');
+      }
+      if (!auth.isResumeAccpeted) {
+        return Waiting();
+      }
+      return redirect('/resume');
+    },
+  },
+  {
+    path: '/',
+    action: ({ redirect }) => {
+      if (auth.isSignedCustomer) {
+        return redirect(`/${auth.uid}`);
+      }
+      return ApplicateForm();
+    },
+  },
+  {
+    path: '/resume',
+    action: ({ redirect }) => {
+      if (!auth.isResumeAccpeted) {
+        return redirect(`/`);
+      }
+      return Resume();
+    },
+  },
+  {
     path: '/login',
     action: ({ redirect }) => {
-      if (isAuthLogin()) {
-        redirect('/admin');
+      if (auth.isSignedAdmin) {
+        return redirect('/admin');
       }
       return LoginForm();
     },
@@ -15,8 +48,8 @@ export default [
   {
     path: '/admin',
     action: ({ redirect }) => {
-      if (!isAuthLogin()) {
-        redirect('/login');
+      if (!auth.isSignedAdmin) {
+        return redirect('/login');
       }
       return AdminBoard();
     },
--- src/server/webApiRouter.ts
+++ src/server/webApiRouter.ts
@@ -4,10 +4,25 @@ import firebase from './firebase';
 
 const app = express();
 
+// Authentication for the admin
 app.post('/auth', (req, res) => {
   return firebase.auth().signInWithEmailAndPassword(req.body.username, req.body.password)
     .then(() => res.redirect('/admin'))
-    .catch(error => res.redirect('/login'));
+    .catch(() => res.redirect('/login'));
+});
+
+// Resume application for the customer
+app.post('/applicate', async (req, res) => {
+  const anonymouslyUser = await firebase.auth().signInAnonymously()
+    .catch(() => res.redirect('/'));
+
+  firebase.database()
+    .ref(`applicate/${anonymouslyUser.uid}`)
+    .set({
+      ...req.body,
+      isResumeAccepted: false,
+    });
+  return res.redirect(`/${anonymouslyUser.uid}`);
 });
 
 export default app;
