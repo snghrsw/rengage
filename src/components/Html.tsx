e0118281f92758afbc28a1c633210b994a44189c
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-09 02:10:07 +0900

    Fix Html to add meta

--- src/components/Html.tsx
+++ src/components/Html.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 export default function Html(content: string) {
   return (
     <html>
+      <head>
+        <meta charSet="UTF-8" />
+      </head>
       <body>
-        <h1>hello world</h1>
         <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
-        <script src="vendor.js" />
-        <script src="bundle.js" />
+        <script src="vendor.js" type="text/babel" />
+        <script src="bundle.js" type="text/babel" />
       </body>
     </html>
   );
