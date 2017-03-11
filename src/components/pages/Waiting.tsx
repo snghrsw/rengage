72e5cb3591553be01fb116a49f36fd29677b5815
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-12 00:47:30 +0900

    Add displasy customer hash url

--- src/components/pages/Waiting.tsx
+++ src/components/pages/Waiting.tsx
@@ -4,6 +4,9 @@ export default function Waiting() {
   return (
     <div>
       <p>snghrymが確認するまでお待ちください</p>
+
+      <label>専用URL</label>
+      <input type="text" value={location.href} />
     </div>
   );
 }
