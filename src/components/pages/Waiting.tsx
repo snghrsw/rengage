14dbb64c1316ad796a15d64a5213933be1b7a02a
Shingo Hirayama <hirayamaru@gmail.com>
2017-03-12 18:17:48 +0900

    Update Waiting component

--- src/components/pages/Waiting.tsx
+++ src/components/pages/Waiting.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
-export default function Waiting() {
+interface IWaiting {
+  locationHref: string;
+}
+
+export default function Waiting(props: IWaiting) {
   return (
     <div>
       <p>snghrymが確認するまでお待ちください</p>
 
       <label>専用URL</label>
-      <input type="text" value={location.href} />
+      <input type="text" value={props.locationHref} />
     </div>
   );
 }
